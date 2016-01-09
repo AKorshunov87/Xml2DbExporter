5ad5e53327c89e25fa95819f63359bfc129dd9f3
AKorshunov87 <a.korshunov87@gmail.com>
2016-01-09 20:55:46 +0300

    Order table model was added
    subj

---
+++ Xml2DbExporter/Data/OrderModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Xml2DbExporter.Data {
+    /// <summary>
+    /// Model for Order table
+    /// </summary>
+    public class OrderModel {
+
+        public long OrderID { get; set; }
+
+        public int CustomerID { get; set; }
+
+        public DateTime? OrderDate { get; set; }
+
+        public string OrderValue { get; set; }
+
+        public int OrderStatus { get; set; }
+
+        public DateTime? DateTimeAdded { get; set; }
+
+        public DateTime? DateTimeUpdated { get; set; }
+
+        public int OrderType { get; set; }
+    }
+}

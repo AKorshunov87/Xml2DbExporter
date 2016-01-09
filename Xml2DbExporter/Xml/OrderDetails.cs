6ac0c9a473c468e04ec23f1107aae4be5d1a64ef
AKorshunov87 <a.korshunov87@gmail.com>
2016-01-09 20:38:22 +0300

    serializable classes added
    Serializable classes were added. Also scheme for validation was added.

---
+++ Xml2DbExporter/Xml/OrderDetails.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Xml.Serialization;
+
+namespace Xml2DbExporter.Xml {
+    [SerializableAttribute]
+    [XmlTypeAttribute(AnonymousType = true)]
+    public class OrderDetails {
+
+        string orderValue;
+        int orderStatus;
+        int orderType;
+
+        [XmlElementAttribute]
+        public string OrderValue {
+            get { return orderValue; }
+            set { orderValue = value; }
+        }
+
+        [XmlElementAttribute]
+        public int OrderStatus {
+            get { return orderStatus; }
+            set { orderStatus = value; }
+        }
+
+        [XmlElementAttribute]
+        public int OrderType {
+            get { return orderType; }
+            set { orderType = value; }
+        }
+    }
+}
---
+++ Xml2DbExporter/Xml/Orders.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Xml.Serialization;
+
+namespace Xml2DbExporter.Xml {
+    [SerializableAttribute]
+    [XmlTypeAttribute(AnonymousType = true)]
+    [XmlRootAttribute(Namespace = "", IsNullable = false)]
+    public class Orders {
+
+        int customerID;
+        DateTime orderDate;
+        OrderDetails[] orderDetailsList;
+
+        [XmlElementAttribute]
+        public int CustomerID {
+            get { return customerID; }
+            set { customerID = value; }
+        }
+
+        [XmlElementAttribute(DataType = "date")]
+        public DateTime OrderDate {
+            get { return orderDate; }
+            set { orderDate = value; }
+        }
+
+        [XmlArrayItemAttribute("OrderDetails", IsNullable = false)]
+        public OrderDetails[] OrderDetailsList {
+            get { return orderDetailsList; }
+            set { orderDetailsList = value; }
+        }
+    }
+}
